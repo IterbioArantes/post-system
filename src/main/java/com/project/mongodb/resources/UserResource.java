3339aea28c98597d157609c0fcfd7f177d74e877
Iterbio Arantes <iterbio7@gmail.com>
2024-05-07 08:07:56 -0300

    Implem User layer

---
+++ src/main/java/com/project/mongodb/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.project.mongodb.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.mongodb.domain.User;
+import com.project.mongodb.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> list = userService.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+}
