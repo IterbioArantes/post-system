b3044fcc343e0d2d4f92337b620585dfc025054b
Iterbio Arantes <iterbio7@gmail.com>
2024-05-07 12:34:53 -0300

    Reference Posts to User

--- src/main/java/com/project/mongodb/config/TestConfig.java
+++ src/main/java/com/project/mongodb/config/TestConfig.java
@@ -44,6 +44,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
+		iterbio.getPosts().addAll(Arrays.asList(post1,post2));
+		
+		userRepository.save(iterbio);
 		
 	}
 
